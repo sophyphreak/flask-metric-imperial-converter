2a8e18e3317f80086627557e359caa7cf13de2d9
Andrew Horn <arhorn@smcm.edu>
2020-04-07 20:22:38 -0500

    added flask-cors

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 from resources.metric_imperial_converter import MetricImperialConverter
 
 app = Flask(__name__)
 Talisman(app)
+CORS(app)
 api = Api(app)
 
 api.add_resource(MetricImperialConverter, '/api/convert/')
